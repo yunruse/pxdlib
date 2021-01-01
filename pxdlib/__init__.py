3f553fe44b46e949811f1e0ab6421541c8954099
Mia Dobson <mia@yunru.se>
2021-01-01 19:02:41 +0000

    Directly expose enums in API

--- pxdlib/__init__.py
+++ pxdlib/__init__.py
@@ -3,6 +3,7 @@ __copyright__ = "Copyright 2020-2021 Mia yun Ruse"
 __status__ = "Alpha 0.0.1"
 __email__ = "mia@yunru.se"
 
+from .enums import *
 from .structure import *
 from .layer import *
 from .pxdfile import *
