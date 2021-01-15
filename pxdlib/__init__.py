1fa24216d1dcfd8b1ab100dfacb01cd52dbf958d
Mia Dobson <mia@yunru.se>
2021-01-15 14:13:09 +0000

    Note `PixelmatorError` in docs

--- pxdlib/__init__.py
+++ pxdlib/__init__.py
@@ -3,6 +3,7 @@ __copyright__ = "Copyright 2020-2021 Mia yun Ruse"
 __status__ = "Alpha 0.0.3"
 __email__ = "mia@yunru.se"
 
+from .errors import *
 from .enums import *
 from .structure import *
 from .styles import *
