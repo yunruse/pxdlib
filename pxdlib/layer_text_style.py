df372d0a452f281951aa1065e412fab011f6db5b
Mia yun Ruse <mia@yunru.se>
2022-12-20 21:00:18 +0000

    add TextStyle.color

--- pxdlib/layer_text_style.py
+++ pxdlib/layer_text_style.py
@@ -1,4 +1,5 @@
 from .plist import NSDictionary
+from .color import Color
 
 TEXTKIT_PREFIX = 'com.pixelmatorteam.textkit.attribute.'
 
@@ -28,10 +29,25 @@ class TextStyle:
     @property
     def is_underline(self):
         return bool(self._get('underline', 0))
+    
+    @property
+    def color(self):
+        desc: bytes = self._get('color').NSComponents
+        color = [float(x) for x in desc.decode().split()]
+        return Color.from_rgb(*color[:3])
+    
+    @color.setter
+    def color(self, color: Color):
+        raise NotImplementedError('Cannot set color')
+        if not isinstance(color, Color):
+            raise TypeError('Can only set color to a Color')
+        self._get('color').NSComponents = '{} {} {} 1'.format(
+            color.r, color.g, color.b).encode()
 
     def _repr_info(self):
         yield self.font_family
         yield self.font_face
+        yield str(self.color)
 
     def __repr__(self):
         info = list(self._repr_info())
